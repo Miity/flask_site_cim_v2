58b23c6c09af8e848400c08b6ac759205625c896
Andreiuk Dmytro <andreiuk.dmytro@gmail.com>
2022-10-13 16:55:19 +0200

    fix

--- flask_site/utils/create_db.py
+++ flask_site/utils/create_db.py
@@ -6,12 +6,12 @@ parrent_dir = os.path.abspath(os.path.join(cur_dir, os.pardir))
 import sys
 sys.path.append(parrent_dir)
 
-from ..app import db
-from ..models.page import Page
-from ..models.user import User
-from ..models.block import Block
-from ..models.image import Image, Gallery
-from ..models.text import Text
+from app import db
+from models.page import Page
+from models.user import User
+from models.block import Block
+from models.image import Image, Gallery
+from models.text import Text
 
 
 db.drop_all()
