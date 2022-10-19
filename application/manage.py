17feb81dfcb8ecac1f6f57ca4f4ba03fdb8a707a
Andreiuk Dmytro <andreiuk.dmytro@gmail.com>
2022-10-19 18:57:01 +0200

    fix

--- application/manage.py
+++ application/manage.py
@@ -2,11 +2,11 @@ from application.app import db
 
 
 def drop_creat_db(db):
-    from models.page import Page
-    from models.user import User
-    from models.block import Block
-    from models.image import Image, Gallery
-    from models.text import Text
+    from application.models.page import Page
+    from application.models.user import User
+    from application.models.block import Block
+    from application.models.image import Image, Gallery
+    from application.models.text import Text
     db.drop_all()
     db.create_all()
     
